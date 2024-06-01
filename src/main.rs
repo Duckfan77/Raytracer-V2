05fad9115694ca545ab880813b4b9eae335e9bae
Sapphire <77photon@gmail.com>
2024-06-01 01:12:08 -0700

    Create First Image
    Created the first image, using ppm output.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!2");
+use std::io::{stdout, Write};
+
+use anyhow::Result;
+
+fn main() -> Result<()> {
+    // Image details
+    let image_width = 256;
+    let image_height = 256;
+
+    // Render
+    let mut stdout = stdout().lock();
+    write!(stdout, "P3\n{image_width} {image_height}\n255\n")?;
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.0;
+
+            let ir = (255.999 * r) as i32;
+            let ig = (255.999 * g) as i32;
+            let ib = (255.999 * b) as i32;
+
+            write!(stdout, "{ir} {ig} {ib}\n")?;
+        }
+    }
+
+    Ok(())
 }
