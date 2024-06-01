4f065f6f4e02ccaddec767424627fcb77377d6dd
Sapphire <77photon@gmail.com>
2024-06-01 16:37:43 -0700

    Add Basic Camera
    Added a basic camera to the main function, with a viewport, focal length,
    aspect ratio, and image width.

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,44 @@ use std::{env, io::Write};
 use anyhow::Result;
 use color::{write_color, Color};
 use image::RgbImage;
+use ray::Ray;
+use vec3::{Point3, Vec3};
 
 mod color;
 mod ray;
 mod vec3;
 
+fn ray_color(_r: &Ray) -> Color {
+    todo!()
+}
+
 fn main() -> Result<()> {
     // Image details
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 480;
+
+    // Calculate image height, ensure it's at least 1
+    let image_height = (image_width as f64 / aspect_ratio) as u32;
+    let image_height = if image_height > 0 { image_height } else { 1 };
+
+    // Camera
+    let focal_length = 1.0; // Distance between the camera center and the viewport
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
+    let camera_center = Point3::new(0.0, 0.0, 0.0);
+
+    // Viewport Vectors
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0); // Horizontal across the viewport, starting at the left
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0); // Vertical across the viewport, starting at the top
+
+    // Viewport pixel delta vectors
+    let pixel_delta_u = viewport_u / image_width as f64;
+    let pixel_delta_v = viewport_v / image_height as f64;
+
+    // Find upper left pixel
+    let viewport_upper_left = // upper left corner of the viewport
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
+    let pixel_00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v); // First pixel is half a pixel delta from the top left corner
 
     // Render
     let mut buf = RgbImage::new(image_width, image_height);
@@ -21,16 +50,14 @@ fn main() -> Result<()> {
         write!(stdout, "\rScanlines remaining: {}", image_height - j)?;
         stdout.flush()?;
         for i in 0..image_width {
-            write_color(
-                &mut buf,
-                &Color::new(
-                    i as f64 / (image_width - 1) as f64,
-                    j as f64 / (image_height - 1) as f64,
-                    0.0,
-                ),
-                i,
-                j,
-            )
+            let pixel_center =
+                pixel_00_loc + (i as f64 * pixel_delta_u) + (j as f64 * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let r = Ray::new(&camera_center, &ray_direction);
+
+            let pixel_color = ray_color(&r);
+
+            write_color(&mut buf, &pixel_color, i, j)
         }
     }
 
