2ed7715d2bd1e8bf43251b28a89fa3ed0c4be858
Sapphire <77photon@gmail.com>
2024-06-01 16:51:32 -0700

    Add initial sphere detection
    Added an initial sphere collision check, fails to check that the sphere is in front of the camera,
    has no shading, or anything else.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,21 @@ mod color;
 mod ray;
 mod vec3;
 
+fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> bool {
+    let oc = *center - *r.origin();
+    let a = r.direction().dot(r.direction());
+    let b = -2.0 * r.direction().dot(&oc);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+
+    discriminant > 0.0
+}
+
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, r) {
+        return Color::new(1.0, 0.0, 0.0);
+    }
+
     // Basic gradient. This is expected to have a small horizontal gradient to go with the vertical gradient,
     // due to normalizing the direction before taking the y coordinate.
 
