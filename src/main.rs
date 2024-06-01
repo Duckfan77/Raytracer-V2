6e39fb1bf01201c70edac633e2e071cc27246337
Sapphire <77photon@gmail.com>
2024-06-01 01:52:36 -0700

    Replace output format with PNG
    Changed the output format from ppm to png, using the image crate.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::io::{stdout, Write};
+use std::env;
 
 use anyhow::Result;
+use image::{Rgb, RgbImage};
 
 fn main() -> Result<()> {
     // Image details
@@ -8,8 +9,7 @@ fn main() -> Result<()> {
     let image_height = 256;
 
     // Render
-    let mut stdout = stdout().lock();
-    write!(stdout, "P3\n{image_width} {image_height}\n255\n")?;
+    let mut buf = RgbImage::new(image_width, image_height);
 
     for j in 0..image_height {
         for i in 0..image_width {
@@ -17,13 +17,20 @@ fn main() -> Result<()> {
             let g = j as f64 / (image_height - 1) as f64;
             let b = 0.0;
 
-            let ir = (255.999 * r) as i32;
-            let ig = (255.999 * g) as i32;
-            let ib = (255.999 * b) as i32;
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
 
-            write!(stdout, "{ir} {ig} {ib}\n")?;
+            buf.put_pixel(i, j, Rgb([ir, ig, ib]))
         }
     }
+
+    buf.save_with_format(
+        &env::args()
+            .nth(1)
+            .expect("Must provide a file to save the image to"),
+        image::ImageFormat::Png,
+    )?;
 
     Ok(())
 }
