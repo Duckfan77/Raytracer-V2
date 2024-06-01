df6a2ca220cd6da7f414b5b28bd84f04ce8e2478
Sapphire <77photon@gmail.com>
2024-06-01 13:29:27 -0700

    Add Progress Indicator
    Added a progress indicator, counting the number of scanlines remaining

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, io::Write};
 
 use anyhow::Result;
 use image::{Rgb, RgbImage};
@@ -11,7 +11,10 @@ fn main() -> Result<()> {
     // Render
     let mut buf = RgbImage::new(image_width, image_height);
 
+    let mut stdout = std::io::stdout().lock();
     for j in 0..image_height {
+        write!(stdout, "\rScanlines remaining: {}", image_height - j)?;
+        stdout.flush()?;
         for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
@@ -21,15 +24,17 @@ fn main() -> Result<()> {
             let ig = (255.999 * g) as u8;
             let ib = (255.999 * b) as u8;
 
-            buf.put_pixel(i, j, Rgb([ir, ig, ib]))
+            buf.put_pixel(i, j, Rgb([ir, ig, ib]));
         }
     }
 
     buf.save_with_format(
-        &env::args()
-            .nth(1)
-            .expect("Must provide a file to save the image to"),
+        &env::args().nth(1).unwrap_or("output/test.png".to_string()),
         image::ImageFormat::Png,
+    )?;
+    write!(
+        stdout,
+        "\rDone.                                                 \n"
     )?;
 
     Ok(())
