0390cfc2928d4ce2ac66e53224bdd57e121f5aa2
Sapphire <77photon@gmail.com>
2024-06-03 08:33:39 -0700

    Add AABB Helper Functions to Interval
    Added some helper functions used to in AABB computations to Interval

--- src/interval.rs
+++ src/interval.rs
@@ -18,6 +18,11 @@ pub trait Clamp {
     fn clamp(&self, x: f64) -> f64;
 }
 
+pub trait AabbHelper {
+    fn overlaps(&self, other: Self) -> bool;
+    fn expand(&self, delta: f64) -> Self;
+}
+
 impl Surrounds for RangeInclusive<f64> {
     fn surrounds(&self, x: f64) -> bool {
         *self.start() < x && x < *self.end()
@@ -33,5 +38,18 @@ impl Clamp for RangeInclusive<f64> {
         } else {
             x
         }
+    }
+}
+
+impl AabbHelper for RangeInclusive<f64> {
+    fn overlaps(&self, other: Self) -> bool {
+        let t_min = f64::max(*self.start(), *other.start());
+        let t_max = f64::min(*self.end(), *other.end());
+        t_min < t_max
+    }
+
+    fn expand(&self, delta: f64) -> Self {
+        let padding = delta / 2.0;
+        (self.start() - padding)..=(self.end() + padding)
     }
 }
